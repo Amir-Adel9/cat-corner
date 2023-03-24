80f1a9c97c6f8bc5f64c064f4fa09f66b14d85c7
Amir-Adel9 <amiradel7152@gmail.com>
2023-03-24 13:49:35 +0200

    Added auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
 import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
@@ -5,7 +7,11 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
