3bd6eedc8edc3d509532a9414feba2a66030b649
Amir-Adel9 <amiradel7152@gmail.com>
2023-03-26 22:34:44 +0200

    Added middleware for handling large images

---
+++ src/pages/api/postMiddleware.ts
@@ -0,0 +1,40 @@
+import { uploadImage } from '~/utils/cloudinary';
+
+import { getPostData } from '~/utils/formidable';
+
+import { type NextApiRequest, type NextApiResponse } from 'next/types';
+
+import { postsRouter } from '~/server/api/routers/posts';
+
+import { prisma } from '~/server/db';
+
+import { getAuth } from '@clerk/nextjs/server';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handlePost(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = getAuth(req);
+  const currentUserId = session.userId;
+  const createPostCaller = postsRouter.createCaller({
+    currentUserId: currentUserId,
+    prisma: prisma,
+  });
+
+  const { postContent, imageBase64 } = await getPostData(req);
+
+  const imageURL = await uploadImage(imageBase64);
+
+  const response = await createPostCaller.createPost({
+    content: postContent,
+    imageURL: imageURL,
+  });
+
+  res.status(200).json(response);
+}
---
+++ src/utils/formidable.ts
@@ -0,0 +1,19 @@
+import { Fields, IncomingForm } from 'formidable';
+import { type NextApiRequest } from 'next';
+
+export async function getPostData(req: NextApiRequest) {
+  const data = await new Promise<{ postContent: string; imageBase64: string }>(
+    function (resolve, reject) {
+      const form = new IncomingForm({ keepExtensions: true });
+      form.parse(req, function (err, fields) {
+        if (err) return reject(err);
+        resolve({
+          postContent: fields.postContent as string,
+          imageBase64: fields.imageBase64 as string,
+        });
+      });
+    }
+  );
+
+  return data;
+}
