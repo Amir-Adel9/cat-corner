7e980a8606c871cfd8355155699377aa8b715c7d
Amir-Adel9 <amiradel7152@gmail.com>
2023-03-31 15:57:25 +0200

    Added and styled likes and comments interfaces

---
+++ src/components/comment.tsx
@@ -0,0 +1,18 @@
+const CommentInterface = (props: { postComments: number }) => (
+  <span className='flex duration-200  items-center group gap-2 cursor-pointer '>
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 32 32'
+      className='duration-200 fill-slate-100 group-hover:fill-[#01A66F] opacity-70 group-hover:opacity-100'
+      width={24}
+      height={24}
+    >
+      <path d='M16 3c-3.79 0-7.234 1.113-9.781 2.977C3.668 7.836 2 10.507 2 13.5c0 3.629 2.473 6.7 6 8.55V29l6.746-5.063c.41.032.824.063 1.254.063 3.79 0 7.234-1.113 9.781-2.973C28.332 19.164 30 16.492 30 13.5s-1.668-5.664-4.219-7.523C23.234 4.113 19.79 3 16 3Zm0 2c3.39 0 6.445 1.016 8.602 2.59C26.758 9.164 28 11.246 28 13.5s-1.242 4.336-3.398 5.91C22.445 20.984 19.39 22 16 22c-.492 0-.984-.027-1.477-.074l-.382-.031L10 25v-4.14l-.578-.266C6.07 19.02 4 16.387 4 13.5c0-2.254 1.242-4.336 3.398-5.91C9.555 6.016 12.61 5 16 5Z' />
+    </svg>
+    <span className='duration-200 group-hover:text-[#01A66F] opacity-70 group-hover:opacity-100'>
+      {props.postComments}
+    </span>
+  </span>
+);
+
+export default CommentInterface;
---
+++ src/components/like.tsx
@@ -0,0 +1,43 @@
+const LikeInterface = (props: { likes: number; isLiked: boolean }) => (
+  <span className='flex duration-200  items-center group gap-2 cursor-pointer '>
+    {!props.isLiked ? (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        className='duration-200 fill-slate-100 group-hover:fill-[#f91880] opacity-70 group-hover:opacity-100'
+        width={24}
+        height={24}
+        {...props}
+      >
+        <path d='M16.5 3C13.605 3 12 5.09 12 5.09S10.395 3 7.5 3A5.5 5.5 0 0 0 2 8.5c0 4.171 4.912 8.213 6.281 9.49C9.858 19.46 12 21.35 12 21.35s2.142-1.89 3.719-3.36C17.088 16.713 22 12.671 22 8.5A5.5 5.5 0 0 0 16.5 3zm-1.689 13.11c-.177.16-.331.299-.456.416-.751.7-1.639 1.503-2.355 2.145a175.713 175.713 0 0 1-2.355-2.145c-.126-.117-.28-.257-.456-.416C7.769 14.827 4 11.419 4 8.5 4 6.57 5.57 5 7.5 5c1.827 0 2.886 1.275 2.914 1.308L12 8l1.586-1.692C13.596 6.295 14.673 5 16.5 5 18.43 5 20 6.57 20 8.5c0 2.919-3.769 6.327-5.189 7.61z' />
+      </svg>
+    ) : (
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 256 256'
+        width={24}
+        height={24}
+        {...props}
+      >
+        <path
+          d='M16.256 3.005C13.515 3.117 12 5.09 12 5.09s-1.515-1.973-4.256-2.085c-1.838-.075-3.523.84-4.633 2.307-3.862 5.104 3.45 11.075 5.17 12.678 1.029.959 2.299 2.098 3.057 2.773a.99.99 0 0 0 1.323 0c.758-.675 2.028-1.814 3.057-2.773 1.72-1.603 9.033-7.574 5.17-12.678-1.109-1.467-2.794-2.382-4.632-2.307z'
+          transform='scale(10.66667)'
+          fill='#f91880'
+          strokeMiterlimit={10}
+          fontFamily='none'
+          fontWeight='none'
+          fontSize='none'
+          textAnchor='none'
+          style={{
+            mixBlendMode: 'normal',
+          }}
+        />
+      </svg>
+    )}
+    <span className='duration-200 group-hover:text-[#f91880] opacity-70 group-hover:opacity-100'>
+      {props.likes}
+    </span>
+  </span>
+);
+
+export default LikeInterface;
